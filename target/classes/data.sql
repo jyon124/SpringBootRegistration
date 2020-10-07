fe99a6144a74474fbd2dfa9253317af7a1dd0d36
Jooyon Won <jyon1204@gmail.com>
2020-10-06 22:46:29 -0700

    #mvn stuff

---
+++ target/classes/data.sql
@@ -0,0 +1,5 @@
+insert into user values (1,'John', 'Cooper', 'jc824@mail.com', 'abcD@1234', '08/24/1990', 'Male');
+insert into user values (2,'Marge', 'Watson', 'mg731@mail.com', 'abcD@1234', '07/31/1990', 'Female');
+insert into user values (3,'Shwarts', 'Pitt', 'mp202@mail.com', 'abcD@1234', '02/02/1995', 'Male');
+insert into user values (4,'Lisa', 'Glen', 'lg115@mail.com', 'abcD@1234', '01/15/1987', 'Female');
+insert into user values (5,'Chloe', 'Hemingway', 'ch413@mail.com', 'abcD@1234', '04/13/1992', 'Female');
