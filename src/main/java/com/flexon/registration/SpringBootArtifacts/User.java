5f60ab601fd9a4cc8cd887c7250197b4263d2171
Jooyon Won <jyon1204@gmail.com>
2020-10-07 12:00:36 -0700

    #CRUD Methods

--- src/main/java/com/flexon/registration/SpringBootArtifacts/User.java
+++ src/main/java/com/flexon/registration/SpringBootArtifacts/User.java
@@ -4,6 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.persistence.Column;
 import javax.persistence.SequenceGenerator;
 
@@ -16,57 +20,68 @@ public class User {
 	@JsonIgnore  //filter this in response
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int user_id;
-	
+	private long user_id;
 	private String firstName;
-	
 	private String lastName;
-	
 	private String email;
-	
 	private String password;
-	
 	private String dateOfBirth;
-	
 	private String gender;
 	
 	public User() {
-		
-	}
-	
-	public User(String firstName, String lastName, String email, String password, String dateOfBirth, String gender) {
 		super();
-		this.setUser_id(0);
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.password = password;
-		this.dateOfBirth = dateOfBirth;
-		this.gender = gender;
+		this.firstName = null;
+		this.lastName = null;
+		this.email = null;
+		this.password = null;
+		this.dateOfBirth = null;
+		this.gender = null;
 	}
 	
 	public String getFirstName() {
 		return firstName;
 	}
 	
-	public void setFirstName(String fn) {
-		this.firstName = fn;
+	public Boolean setFirstName(String fn) {
+		if(fn.length() > 2 && fn != null && fn.matches("^[a-zA-Z]*$")) {
+			this.firstName = fn;
+			System.out.println("Successfully stored: " + fn);
+			return true;
+		} else {
+			System.out.println("Please enter a valid first name that is longer than two characters without any number and special characters.");
+			return false;
+		}
 	}
 	
 	public String getLastName() {
 		return lastName;
 	}
 	
-	public void setLastName(String ln) {
-		this.lastName = ln;
+	public Boolean setLastName(String ln) {
+		if(ln.length() > 2 && ln != null && ln.matches("^[a-zA-Z]*$")) {
+			this.lastName = ln;
+			System.out.println("Successfully stored: " + ln);
+			return true;
+		} else {
+			System.out.println("Please enter a valid last name that is longer than two characters without any number and special characters.");
+			return false;
+		}
 	}
 	
 	public String getEmail() {
 		return email;
 	}
 	
-	public void setEmail(String email) {
-	  	this.email = email;
+	public Boolean setEmail(String email) {
+	      String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+	      if(email.matches(regex)){
+	  		this.email = email;
+			System.out.println("Successfully stored: " + email);
+			return true;
+	      } else {
+	    	System.out.println("Please Enter a valid email format.");
+	    	return false;
+	      }
 	}
 	
 	public String getPassword() {
@@ -74,35 +89,68 @@ public class User {
 	}
 	
 	public void setPassword(String password) {
-		this.password = password;
+//		String regex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{8,20}$";
+//		boolean validPassword = isValidPassword(password,regex);
+//		if(validPassword) {
+			this.password = password;
+//			System.out.println("Successfully stored: " + password);
+//			return true;
+//		} else {
+//            System.out.println("Password must be less than 20 and more than 8 characters in length, at least one uppercase character, one lowercase character, one number, and one special character among @#$%.");
+//            return false;
+//		}
+	}
+	
+	public static boolean isValidPassword(String password,String regex)
+	{
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(password);
+		return matcher.matches();
 	}
 	
 	public String getDateOfBirth() {
 		return dateOfBirth;
 	}
 	
-	public void setDateOfBirth(String dateOfBirth) {
-		this.dateOfBirth = dateOfBirth;
+	public Boolean setDateOfBirth(String dateOfBirth) {
+		if(isValidDate(dateOfBirth)) {
+			this.dateOfBirth = dateOfBirth;
+			System.out.println("Successfully stored: " + dateOfBirth);
+			return true;
+		} else {
+			System.out.println("Please enter valid date of birth format.");
+			System.out.println("ex) mm/dd/yyyy");
+			return false;
+		}
 	}
+	
+    public static boolean isValidDate(String d) 
+    { 
+        String regex = "^(1[0-2]|0[1-9])/(3[01]" + "|[12][0-9]|0[1-9])/[0-9]{4}$"; 
+        Pattern pattern = Pattern.compile(regex); 
+        Matcher matcher = pattern.matcher((CharSequence)d); 
+        return matcher.matches(); 
+    } 
 	
 	public String getGender() {
 		return gender;
 	}
 	
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-
-	public int getUser_id() {
-		return user_id;
-	}
-
-	public void setUser_id(int user_id) {
-		this.user_id = user_id;
-	}
-	
-	@Override
-	public String toString() {
-		return "User [userID=" + user_id + ", First Name=" + firstName + ", Last Name=" + lastName + ", email="	+ email + ", Date of Birth=" + dateOfBirth + ","  + " Gender=" + gender + "]";
+	public Boolean setGender(String gender) {
+		String lowGender = gender.toLowerCase();
+		if(lowGender.equals("m") || lowGender.equals("male")) {
+			this.gender = "Male";
+			System.out.println("Successfully stored: " + this.gender);
+			return true;
+		} else if(lowGender.equals("f") || lowGender.equals("female")) {
+			this.gender = "Female";
+			System.out.println("Successfully stored: " + this.gender);
+			return true;
+		}
+		else {
+			System.out.println("Please enter a valid gender");
+			System.out.println("ex) Enter 'M' or 'Male' for male, 'F' or 'Female' for female");
+			return false;
+		}
 	}
 }
--- src/main/java/com/flexon/registration/SpringBootArtifacts/UserController.java
+++ src/main/java/com/flexon/registration/SpringBootArtifacts/UserController.java
@@ -1,24 +1,21 @@
 package com.flexon.registration.SpringBootArtifacts;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sun.el.stream.Optional;
 
 @RestController
 public class UserController {
@@ -32,9 +29,22 @@ public class UserController {
 	}
 	
 	@PostMapping(path="/jpa/addNewUser")
-	public ResponseEntity<String> addNewStudent(@RequestBody User newUser){
+	public ResponseEntity<String> addNewUser(@RequestBody User newUser){
 		userRepo.save(newUser);
 		return new ResponseEntity<>("Created new User",HttpStatus.CREATED);
 	}
 	
+	@PutMapping(path="/jpa/updatePw/{id}")
+	public ResponseEntity<String> changePassword(@RequestBody String newPassword, @PathVariable Long id) {
+		java.util.Optional<User> user = userRepo.findById(id);
+		user.get().setPassword(newPassword);
+		System.out.println(user.get().getPassword());
+		return new ResponseEntity<>("Password updated",HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping(path="/jpa/deleteUser/{id}")
+	public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+		userRepo.deleteById(id);
+		return new ResponseEntity<>("User ID:" + id + "has been deleted.",HttpStatus.ACCEPTED);
+	}
 }
